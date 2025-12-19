20d017dd9d3903912bdf3a0cca1bc920acaed71e
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-19 17:18:50 -0300

    nextjs gerenciamento escolar-deploy: fix: build font wolf2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,37 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = localFont({
+  src: [
+    { path: "../fonts/Inter-Regular.woff2", weight: "400" },
+    { path: "../fonts/Inter-Medium.woff2", weight: "500" },
+    { path: "../fonts/Inter-SemiBold.woff2", weight: "600" },
+    { path: "../fonts/Inter-Bold.woff2", weight: "700" },
+  ],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "Plataforma Ethos CPAC Escola de Psícologia",
+  title: "Plataforma Ethos CPAC Escola de Psicologia",
   description: "Ethos CPAC Escola de psicologia",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="pt-BR">
         <body className={inter.className}>
-          {children} <ToastContainer position="bottom-right" theme="dark" />
+          {children}
+          <ToastContainer position="bottom-right" theme="dark" />
         </body>
       </html>
     </ClerkProvider>
