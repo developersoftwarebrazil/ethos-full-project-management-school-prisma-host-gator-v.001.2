8a9c9f763a7bd93700ced77e393f19278691a9b4
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-19 17:45:21 -0300

    nextjs gerenciamento escolar-deploy: fix: modificando o path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 const inter = localFont({
   src: [
-    { path: "../fonts/Inter-Regular.woff2", weight: "400" },
+    { path: "../fonts/Inter_18pt-Regular.woff2", weight: "400" },
     { path: "../fonts/Inter-Medium.woff2", weight: "500" },
-    { path: "../fonts/Inter-SemiBold.woff2", weight: "600" },
-    { path: "../fonts/Inter-Bold.woff2", weight: "700" },
+    { path: "../fonts/Inter_24pt-SemiBold.woff2", weight: "600" },
+    { path: "../fonts/Inter_18pt-Bold.woff2", weight: "800" },
   ],
   display: "swap",
 });
