55d650055073f80132557138f8f0d55b4c744c22
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2026-01-05 13:50:46 -0300

    nextjs gerenciamento escolar-deploy:add landing page.v1.1

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,27 +1,27 @@
-// // src/app/(public)/layout.tsx  OU  src/app/layout.tsx
-// import type { Metadata } from "next";
-// import { chathura, openSans, roboto } from "@/functions/fonts";
+// src/app/(public)/layout.tsx  OU  src/app/layout.tsx
+import type { Metadata } from "next";
+import { chathura, openSans, roboto } from "@/functions/fonts";
 
-// export const metadata: Metadata = {
-//   title: "ETHOS – Cursos Integrados",
-//   description: "Plataforma de gestão educacional ETHOS",
-// };
+export const metadata: Metadata = {
+  title: "ETHOS – Cursos Integrados",
+  description: "Plataforma de gestão educacional ETHOS",
+};
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{ children: React.ReactNode }>) {
-//   return (
-//     <html lang="pt-BR">
-//       <body
-//         className={`${chathura.variable} ${openSans.variable} ${roboto.variable}`}
-//       >
-//         <div className="App">
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${chathura.variable} ${openSans.variable} ${roboto.variable}`}
+      >
+        <div className="App">
           
-//           <main className="AppBody">
-//             <div className="AppContent">{children}</div>
-//           </main>
-//         </div>
-//       </body>
-//     </html>
-//   );
-// }
+          <main className="AppBody">
+            <div className="AppContent">{children}</div>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
