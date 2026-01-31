21ce934fbe4137f40a644aef92e2ab45da84e70e
CURSOS ORIGAMID <paulocursosorigamid@gmail.com>
2026-01-31 11:58:55 -0300

    nextjs gerenciamento escolar-deploy: upload small videos.v2

--- src/app/api/test/cloudinary/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import cloudinary from "@/lib/cloudinary";
-
-export async function GET() {
-  const res = await cloudinary.api.ping();
-  return NextResponse.json(res);
-}
